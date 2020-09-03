4911456343048c6bc1b761e34dcaa106a3aa9760
drumilT <drumiltrivedi@gmail.com>
2020-09-03 22:35:45 +0530

    add fix for m=1

--- A1/170020016-golomb.py
+++ A1/170020016-golomb.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 ap = argparse.ArgumentParser()
 
@@ -8,5 +9,27 @@ ap.add_argument("-m", required=True,
 ap.add_argument("-n", required=True,
            help="second operand")
 args = vars(ap.parse_args())
+
+n = int(args["n"])
+m = int(args["m"])
+
+rem = int(n%m)
+quo = int(n/m)
+
+out = "".join(["1" for _ in range(quo)])+"0"
+b = int(math.log(m,2))+1
+cut_off = math.pow(2,b)-m
+    
+if rem < cut_off:
+    rem_bin = str(bin(rem).replace("0b", ""))
+    rem_bin= "".join(["0" for _ in range(1,b-len(rem_bin))])+rem_bin
+else:
+    rem += int(cut_off)
+    rem_bin = str(bin(rem).replace("0b", ""))
+    rem_bin= "".join(["0" for _ in range(1,b+1-len(rem_bin))])+rem_bin
+if m==1:
+    rem_bin =""
+
+print(out+rem_bin)
 
 
