8e306e8f825ce8afc7943abf2f348be4436f303e
drumilT <drumiltrivedi@gmail.com>
2020-09-03 19:42:21 +0530

    gamma complete

--- A1/170020016-gamma.py
+++ A1/170020016-gamma.py
@@ -7,4 +7,14 @@ ap.add_argument("-n", required=True,
            help="first operand")
 args = vars(ap.parse_args())
 
+n = int(args["n"])
+
+binary_form = str(bin(n).replace("0b", ""))
+offset = binary_form[1:]
+length = "".join(["1" for _ in range(1,len(binary_form))]) + "0"
+print(length+", "+offset)
+
+
+
+
 
